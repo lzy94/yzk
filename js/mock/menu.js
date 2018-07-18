9199010487a8e512edd34204ff2389cfebea8e93
lzy <15196260302@163.com>
2018-07-18 23:11:09 +0800

    .

--- js/mock/menu.js
+++ js/mock/menu.js
@@ -20,45 +20,47 @@ var menu = [{
 		icon: 'icon-yonghushezhi',
 		childs: ''
 	}]
-}, {
+},{
 	title: '题库系统',
 	path: 'dropdown-tkxt',
 	icon: 'icon-kaodianqingdan',
-	childs: [{
-		title: '题库基础信息管理',
-		path: 'dropdown-tkjcxxgl',
-		icon: 'icon-kaodianqingdan',
-		childs: [{
-			title: '试题类型管理',
-			path: '/tksys/testType.html',
-			icon: 'icon-kaodianqingdan',
-			childs: ''
-		}, {
-			title: '题目类型管理',
-			path: '/tksys/titleType.html',
-			icon: 'icon-kaodianqingdan',
-			childs: ''
-		}, {
-			title: '岗位管理',
-			path: '/tksys/postManagement.html',
-			icon: 'icon-kaodianqingdan',
-			childs: ''
-		}, {
-			title: '职务管理',
-			path: '/tksys/jobManagement.html',
-			childs: ''
-		}, {
-			title: '试卷标识管理',
-			path: '/tksys/identification.html',
-			icon: 'icon-kaodianqingdan',
-			childs: ''
-		}, {
-			title: '实操键值管理',
-			path: '/tksys/practicalManagement.html',
-			icon: 'icon-kaodianqingdan',
-			childs: ''
-		}]
-	}, {
+	childs: [
+//	{
+//		title: '题库基础信息管理',
+//		path: 'dropdown-tkjcxxgl',
+//		icon: 'icon-kaodianqingdan',
+//		childs: [{
+//			title: '试题类型管理',
+//			path: '/tksys/testType.html',
+//			icon: 'icon-kaodianqingdan',
+//			childs: ''
+//		}, {
+//			title: '题目类型管理',
+//			path: '/tksys/titleType.html',
+//			icon: 'icon-kaodianqingdan',
+//			childs: ''
+//		}, {
+//			title: '岗位管理',
+//			path: '/tksys/postManagement.html',
+//			icon: 'icon-kaodianqingdan',
+//			childs: ''
+//		}, {
+//			title: '职务管理',
+//			path: '/tksys/jobManagement.html',
+//			childs: ''
+//		}, {
+//			title: '试卷标识管理',
+//			path: '/tksys/identification.html',
+//			icon: 'icon-kaodianqingdan',
+//			childs: ''
+//		}, {
+//			title: '实操键值管理',
+//			path: '/tksys/practicalManagement.html',
+//			icon: 'icon-kaodianqingdan',
+//			childs: ''
+//		}]
+//	}, 
+	{
 		title: '虚拟公司管理',
 		path: 'dropdown-xngsgl',
 		icon: 'icon-kaodianqingdan',
@@ -74,12 +76,12 @@ var menu = [{
 			childs: ''
 		}, {
 			title: '供应商管理',
-			path: '#',
+			path: '/src/xngs/supplier.html',
 			icon: 'icon-kaodianqingdan',
 			childs: ''
 		}, {
 			title: '维修商管理',
-			path: '#',
+			path: '/src/xngs/maintainer.html',
 			icon: 'icon-kaodianqingdan',
 			childs: ''
 		}, {
@@ -97,12 +99,14 @@ var menu = [{
 		title: '试题管理',
 		path: 'dropdown-stgl',
 		icon: 'icon-kaodianqingdan',
-		childs: [{
-			title: '试题信息管理',
-			path: '#',
-			icon: 'icon-kaodianqingdan',
-			childs: ''
-		}, {
+		childs: [
+//		{
+//			title: '试题信息管理',
+//			path: '#',
+//			icon: 'icon-kaodianqingdan',
+//			childs: ''
+//		},
+		{
 			title: '选择题管理',
 			path: '/testQuestions/choiceQuestion.html',
 			icon: 'icon-kaodianqingdan',
@@ -211,17 +215,19 @@ var menu = [{
 			//			childs: ''
 			//		}
 		]
-	}, {
-		title: '成绩管理',
-		path: '#',
-		icon: 'icon-shitiguanli',
-		childs: [{
-			title: '成绩管理',
-			path: '#',
-			icon: 'icon-shitiguanli',
-			childs: ''
-		}]
-	}]
+	}
+//	, {
+//		title: '成绩管理',
+//		path: '#',
+//		icon: 'icon-shitiguanli',
+//		childs: [{
+//			title: '成绩管理',
+//			path: '#',
+//			icon: 'icon-shitiguanli',
+//			childs: ''
+//		}]
+//	}
+	]
 }, {
 	title: '系统管理',
 	path: '#',
